4cfdb2431083f4fd3a9cf8c8ab68bfb9ca01714f
UsaidAl <ali_usaid98@hotmail.com>
2024-01-24 16:52:37 +0000

    Conflict merging

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,9 @@ const router=require("express").Router()
 const userRoutes=require("./userRoutes.js")
 const categoryRoutes=require("./categoryRoutes.js")
 const productRoutes=require("./productRoutes.js")
-router.use("/",userRoutes)
-router.use("/",categoryRoutes)
-router.use("/",productRoutes)
+router.use("/users",userRoutes)
+router.use("/categories",categoryRoutes)
+router.use("/products",productRoutes)
 //routes go here
 
 module.exports=router
